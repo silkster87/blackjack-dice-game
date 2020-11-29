093363ec626ffc8b8a611ffc529af9d181b6ee50
Silky Ng <silky.ng@gmail.com>
2020-11-29 17:29:36 +0000

    Added Model and Component features:w

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Game } from './models/game.model';
+import { GameService } from './services/game.service';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'blackjack-dice-game';
+
+   p1Turn: boolean;
+   p2Turn: boolean;
+
+   player1Game: Game;
+   player2Game: Game;
+  
+   constructor(private gameService: GameService) {
+     this.gameService = gameService;
+     this.player1Game = this.gameService.getPlayer1Game();
+     this.p1Turn = this.player1Game.Turn;
+
+     this.player2Game = this.gameService.getPlayer2Game();
+     this.p2Turn = this.player2Game.Turn;
+   }
+   
+  handlePlayer1Sticked(totalScore1: number):void{
+    console.log(`Player 1 sticked with score ${totalScore1}`);
+    this.p2Turn = true;
+  }
+
+  handlePlayer2Sticked(totalScore2: number):void{
+   // console.log(`Player 2 sticked with score ${totalScore2} and Player 1 turn is ${this.p1Turn}, Player 2 turn is ${this.p2Turn}`);
+    this.p1Turn = true; //QUESTION - this doesn't display player 1's buttons, why?
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Player1Component } from './player1/player1.component';
+import { Player2Component } from './player2/player2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Player1Component,
+    Player2Component
   ],
   imports: [
     BrowserModule
